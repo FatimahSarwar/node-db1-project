ad4fbe69b5fa500e27299dc2a32d35be9d724afe
Fatimah Sarwar <fatimahsarwar@fatimahs-mbp.myfiosgateway.com>
2022-03-21 19:42:04 -0400

    finished

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,62 @@
+const Accounts = require('./accounts-model')
 const router = require('express').Router()
+const {checkAccountPayload,
+  checkAccountNameUnique,
+  checkAccountId} = require('./accounts-middleware')
+const dbConfig = require('../../data/db-config')
+
+
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  Accounts.getAll()
+  .then(accounts => {
+    res.status(200).json(accounts)
+  })
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  res.status(200).json(req.account)
 })
 
-router.post('/', (req, res, next) => {
+router.post('/',checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
   // DO YOUR MAGIC
+  Accounts.create(req.body)
+  .then(newAccount => {
+      res.status(201).json(newAccount)
+    } 
+  ).catch(err => {
+    next(err)
+  })
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id',checkAccountId, checkAccountPayload, checkAccountNameUnique, (req, res, next) => {
   // DO YOUR MAGIC
+  Accounts.updateById(req.params.id, req.body)
+  .then(updatedAccount => {
+    res.json(updatedAccount)
+  })
+  .catch(err => {
+    next(err)
+  })
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAccountId,(req, res, next) => {
   // DO YOUR MAGIC
+  Accounts.deleteById(req.params.id)
+  .then(res.json(req.account))
+  .catch(err => {
+    next(err)
+  })
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+  })
 })
 
 module.exports = router;
