957cd1bb1f65ade7c925ea51a200d1194a120463
Fatimah Sarwar <fatimahsarwar@fatimahs-mbp.myfiosgateway.com>
2022-03-21 17:15:26 -0400

    finished with model functions

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,35 @@
+const knex = require("knex");
+const config = require('../../knexfile')
+const db = knex(config.development);
+
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db("accounts")
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  return db('accounts').where('id', id).first()
+
 }
 
 const create = account => {
   // DO YOUR MAGIC
+  const [id] =  db('accounts').insert({account})
+  return getById(id)
 }
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
+  db("accounts").where("id", id).update({ account })
+  return getById(id)
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  db("accounts").where("id", id).del()
+  return getById(id)
 }
 
 module.exports = {
